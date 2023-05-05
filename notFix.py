538769c078779f611dcc44066aa857a6d3a65a79
Komang Gede Narariya Suputra <91647402+16Komang@users.noreply.github.com>
2023-05-05 15:15:21 +0700

    fixing processing data

--- notFix.py
+++ notFix.py
@@ -4,13 +4,13 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Contoh Program Apache Spark").getOrCreate()
 
 # baca file CSV
-df = spark.read.format("csv").option("header", "true").load("file:///home/cloudera/spark-2.0.0-bin-hadoop2.7/death-rates-from-air-pollution.csv")
+df = spark.read.csv("file:///home/cloudera/spark-2.0.0-bin-hadoop2.7/death-rates-from-air-pollution.csv", header=True, inferSchema=True)
 
 # filter data untuk daerah Indonesia
 df = df.filter(df.Entity == "Indonesia")
 
 # hapus kolom-kolom yang tidak dibutuhkan
-df = df.drop("Code", "Entity")
+df = df.drop("Code")
 
 # ubah nama kolom
 df = df.withColumnRenamed("Year", "Tahun").withColumnRenamed("Air pollution (total) (deaths per 100,000)", "Jumlah Kematian")
